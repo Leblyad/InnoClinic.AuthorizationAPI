5d280b26fa613f41c89d0b5006a4f83f44b23b8c
S9392929 <80668687+Leblyad@users.noreply.github.com>
2022-11-21 14:49:29 +0300

    Add one role to each user

--- AuthorizationAPI/Application/Serrvices.Abstractions/IAccountService.cs
+++ AuthorizationAPI/Application/Serrvices.Abstractions/IAccountService.cs
@@ -5,7 +5,7 @@ namespace AuthorizationAPI.Application.Serrvices.Abstractions
     public interface IAccountService
     {
         public Task<AuthenticatedUserInfo> AuthenticateUser(UserForAuthenticationDto user);
-        public Task CreateUser(UserForCreationDto userForCreation);
+        public Task CreateUser(UserForCreationDto userForCreation, string role = "Pacient");
         public Task AddRoleToUser(string login, string role);
         public Task SignOutUser();
     }
